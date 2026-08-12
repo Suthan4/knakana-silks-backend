b1f97ebe1061cc0c7412c37837f5639d40bae0a3
Ubuntu <ubuntu@ip-172-31-9-114.ap-south-1.compute.internal>
2026-08-12 08:37:17 +0000

    reset QA Prisma migration baseline

--- prisma/migrations/20260124045618_add_payment_details/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "payments" ADD COLUMN     "bankName" TEXT,
-ADD COLUMN     "cardLast4" TEXT,
-ADD COLUMN     "cardNetwork" TEXT,
-ADD COLUMN     "cardType" TEXT,
-ADD COLUMN     "upiId" TEXT,
-ADD COLUMN     "walletName" TEXT;
